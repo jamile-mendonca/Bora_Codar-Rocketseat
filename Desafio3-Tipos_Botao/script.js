83528042dd84ec514c6cd885159c53241c653776
Jamile-Xavier <jamile.x.m@gmail.com>
2023-01-23 17:08:37 -0300

    Conclusão desafio 3

--- Desafio3-Tipos_Botao/script.js
+++ Desafio3-Tipos_Botao/script.js
@@ -1,1 +1,40 @@
+function changeColor(name) {
 
+
+  const btn = document.getElementsByClassName(name);
+
+  if (btn[0]) {
+
+    switch (name) {
+      case 'btn-int-1':
+        btn[0].style.backgroundColor = "#64436b";
+        break;
+      case 'btn-int-2':
+        btn[0].style.backgroundColor = "#64436b";
+        btn[0].style.cursor = "not-allowed";
+        break;
+      case 'btn-int-3':
+        btn[0].style.backgroundColor = "#64436b";
+        btn[0].style.cursor = "move";
+        break;
+    }
+  }
+}
+
+function backBtn(name) {
+  const btn = document.getElementsByClassName(name);
+  if (btn[0]) {
+
+    switch (name) {
+      case 'btn-int-1':
+        btn[0].style.backgroundColor = "#8257E5";
+        break;
+      case 'btn-int-2':
+        btn[0].style.backgroundColor = "#3C3748";
+        break;
+      case 'btn-int-3':
+        btn[0].style.backgroundColor = "#130F1E";
+        break;
+    }
+  }
+}
